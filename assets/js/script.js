be75d0a266db6bec092a7c1da984a036317f0285
Marzena-W <marzena.m.wilk@gmail.com>
2023-01-27 21:34:13 +0000

    divided code to two functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
-$("#search-button").on("click", function (event) {
-    event.preventDefault();
-
-    var city = $("#search-input").val();
+function showWeather() {
+    var city = $("#search-input").val().trim();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e3fca67d9cc333a831026c5f07c8ba92";
-
 
     $.ajax({
         url: queryURL,
@@ -18,12 +15,6 @@ $("#search-button").on("click", function (event) {
 
         var today = moment().format("L");
 
-
-        // var iconPath = response.weather.icon;
-        // var iconUrl = "https://openweathermap.org/img/wn/" + iconPath + ".png";
-        // var icon = $("<img>").attr("src", iconUrl);
-
-
         var cityName = $("<h4 class='cityname'>").text(city + " (" + today + ")");
         var tempTag = $("<p>").text("Temp: " + tempC + " °C");
         var windTag = $("<p>").text("Wind: " + wind + " KPH");
@@ -34,31 +25,16 @@ $("#search-button").on("click", function (event) {
         $("#today").append(windTag);
         $("#today").append(humidityTag);
 
-
+        ("#search-inputt").val("")
     })
-})
+}
 
+function renderCity() {
+    $("#search-button").on("click", function (event) {
+        event.preventDefault();
+        showWeather()
+        $("#search-input").val("")
+    })
+};
 
-
-
-
-
-
-
-// on click of a button
-    // take the input (city name) from search form and
-        // show current day result in #today by
-            // creating header <h3> with city name, date, icon
-            // creating 3x <p> for: Temp, Wind, Humidity
-            // append h3 and p to #today
-
-        // show 5-day forecast in #forecast
-            // create 5 divs(?)
-            // create date
-            // icon
-            // 3x <p> for: Temp, Wind, Humidity
-            // append to all created to #forecast
-
-    // save data to local storage
-        // create button with city name
-        // save button in #history
+renderCity();
